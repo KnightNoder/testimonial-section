93b6556e9d51d915d948bae4bb4f617bf25252aa
Nasim Akhtar <nasim.akhtar@ghc.health>
2022-05-23 15:49:27 +0530

    added content

--- src/Layouts/Components/TestimonialSlider/TestimonialSlider.jsx
+++ src/Layouts/Components/TestimonialSlider/TestimonialSlider.jsx
@@ -9,76 +9,33 @@ class TestimonialSlider extends Component {
             testimonyDetails: [
                 {
                     name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    image: "https://cdn.shopify.com/s/files/1/0477/0345/7956/files/withoutlogo3_1_180x.jpg?v=1613127251%20180w%20180h,%20//cdn.shopify.com/s/files/1/0477/0345/7956/files/withoutlogo3_1.jpg?v=1613127251%20200w%20200h",
                     description:
                         "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
                 },
                 {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+                    name: "Sachin D'souza",
+                    image: "https://cdn.shopify.com/s/files/1/0477/0345/7956/files/1_180x.jpg?v=1613127309%20180w%20180h,%20//cdn.shopify.com/s/files/1/0477/0345/7956/files/1.jpg?v=1613127309%20200w%20200h",
                     description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
+                        "I am so thankful to GHC for helping me with my struggle with Erectile Dysfunction in a cost effective and discrete manner. ",
                 },
                 {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    name: "Shiva",
+                    image: "https://cdn.shopify.com/s/files/1/0477/0345/7956/files/20210102_152952_180x.jpg?v=1613127201%20180w%20180h,%20//cdn.shopify.com/s/files/1/0477/0345/7956/files/20210102_152952.jpg?v=1613127201%20200w%20200h",
                     description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
+                        "It fits right in with my daily routine. No extra time washing hair or adding products throughout the day. Everything is sent to your door at the perfect time and results have been great at just 2 months in!",
                 },
                 {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    name: "Rachit Gupta",
+                    image: "https://cdn.shopify.com/s/files/1/0477/0345/7956/files/WhatsApp_Image_2021-02-05_at_6.29.12_PM_2_180x.jpg?v=1613127426%20180w%20180h,%20//cdn.shopify.com/s/files/1/0477/0345/7956/files/WhatsApp_Image_2021-02-05_at_6.29.12_PM_2.jpg?v=1613127426%20200w%20200h",
                     description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
+                        "My hair loss is genetics so now I just have to maintain my regrowth. Right now, I am on the hair maintenance kit which includes a serum, oil and shampoo. I am so happy with the results.",
                 },
                 {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+                    name: "Harsh Sharma",
+                    image: "https://cdn.shopify.com/s/files/1/0477/0345/7956/files/shampoo_IMG_23821612169958133_4_180x.jpg?v=1613127442%20180w%20180h,%20//cdn.shopify.com/s/files/1/0477/0345/7956/files/shampoo_IMG_23821612169958133_4.jpg?v=1613127442%20200w%20200h",
                     description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
-                },
-                {
-                    name: "Harpreet Singh",
-                    image: "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                    description:
-                        "My 3-year marriage was going down the drain, you guys have done me a huge favour for making this sensitive topic comfortable and providing great support I needed. This literally saved my marriage and the spark is back.",
+                        "What an amazing change in my hair. I had a lot of hair fall 6 months ago, after I used GHC products my hair fall problems have become much better. I have been using a Hair regrowth kit  for 2 months and the kit has really done wonders for me overall.",
                 },
             ],
         };
@@ -87,7 +44,7 @@ class TestimonialSlider extends Component {
     render() {
         return (
             <Carousel
-                show={window.screen.width > 820 ? 3 : 1}
+                show={window.screen.width > 820 ? 3 : 1.25}
                 slide={1}
                 swiping={true}
                 useArrowKeys={false}
